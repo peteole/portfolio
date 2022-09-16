17c55aa275efdc7210ae0c01a29267f88f3bf034
Ole Petersen <peteole2707@gmail.com>
2022-09-16 14:54:01 +0200

    Added awards

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,61 +18,85 @@ export const getStaticProps: GetStaticProps<{ jsonResume: ResumeSchema }> = () =
 const Home: NextPage<{ jsonResume: ResumeSchema }> = ({ jsonResume }) => {
 
   return (
-    <div >
-      <Container>
-        <h1>Hi, I am {jsonResume.basics?.name}</h1>
-        <Text>{jsonResume.basics?.summary}</Text>
+    <Container css={{ textAlign: "center" }}>
+      <h1>Hi, I am {jsonResume.basics?.name}</h1>
+      <Text>{jsonResume.basics?.summary}</Text>
 
-        <Text h2>Education</Text>
+      <Text h2>Education</Text>
+      <Grid.Container gap={2} justify="center">
+        {jsonResume.education?.map((v, i) => (
+          <Grid key={i}>
+            <Card isHoverable css={{ width: "400px" }}>
+              <Card.Header>
+                <Col>
+                  <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
+                  <Text h3>{v.institution}</Text>
+                </Col>
+              </Card.Header>
+              <Card.Body>
+                <Text>Score: {v.score}</Text>
+                {v.courses && <Container ><Text h4>Courses</Text>{v.courses.map((c, i) => <Badge key={i}>{c}</Badge>)}</Container>}
+              </Card.Body>
+              {v.url && <Card.Footer><Link href={v.url}>Learn more</Link></Card.Footer>}
+            </Card>
+          </Grid>))}
+      </Grid.Container>
+
+      {jsonResume.work && <>
+        <Text h2>Experience</Text>
         <Grid.Container gap={2} justify="center">
-          {jsonResume.education?.map((v, i) => (
+          {jsonResume.work.map((v, i) => <>
             <Grid key={i}>
               <Card isHoverable css={{ width: "400px" }}>
                 <Card.Header>
                   <Col>
-                    <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
-                    <Text h3>{v.institution}</Text>
+                    <Row>
+                      <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
+                      <Badge color="primary">{v.position}</Badge></Row>
+                    <Text h3>{v.name}</Text>
                   </Col>
                 </Card.Header>
                 <Card.Body>
-                  <Text>Score: {v.score}</Text>
-                  {v.courses && <Container ><Text h4>Courses</Text>{v.courses.map((c, i) => <Badge key={i}>{c}</Badge>)}</Container>}
+                  <Text>{v.summary}</Text>
+                  <Text>{v.description}</Text>
+                  {v.highlights && <Container >
+                    <Text h4>Highlights</Text>
+                    {v.highlights.map((c, i) => <Text key={i}>- {c}</Text>)}
+                  </Container>}
                 </Card.Body>
                 {v.url && <Card.Footer><Link href={v.url}>Learn more</Link></Card.Footer>}
               </Card>
-            </Grid>))}
+            </Grid>
+          </>)}
         </Grid.Container>
+      </>}
 
-        {jsonResume.work && <>
-          <Text h2>Experience</Text>
-          <Grid.Container gap={2} justify="center">
-            {jsonResume.work.map((v, i) => <>
-              <Grid key={i}>
-                <Card isHoverable css={{ width: "400px" }}>
-                  <Card.Header>
-                    <Col>
-                      <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
-                      <Text h3>{v.name}</Text>
-                      <Badge>{v.position}</Badge>
-                    </Col>
-                  </Card.Header>
-                  <Card.Body>
-                    <Text>{v.summary}</Text>
-                    <Text>{v.description}</Text>
-                    {v.highlights && <Container >
-                      <Text h4>Highlights</Text>
-                      {v.highlights.map((c, i) => <Text key={i}>- {c}</Text>)}
-                    </Container>}
-                  </Card.Body>
-                  {v.url && <Card.Footer><Link href={v.url}>Learn more</Link></Card.Footer>}
-                </Card>
-              </Grid>
-            </>)}
-          </Grid.Container>
-        </>}
+      {jsonResume.awards && <>
+        <Text h2>Awards</Text>
+        <Grid.Container gap={2} justify="center">
+          {jsonResume.awards.map((v, i) => <>
+            <Grid key={i}>
+              <Card isHoverable css={{ width: "400px" }}>
+                <Card.Header>
+                  <Col>
+                    <Row>
+                      <Badge color="primary" >{v.date}</Badge>
+                      <Badge color="primary">{v.awarder}</Badge>
+                    </Row>
+                    <Text h3>{v.title}</Text>
+                  </Col>
+                </Card.Header>
+                <Card.Body>
+                  <Text>{v.summary}</Text>
+                </Card.Body>
+                {typeof v['url'] == "string" && <Card.Footer><Link href={v['url']}>Learn more</Link></Card.Footer>}
+              </Card>
+            </Grid>
+          </>)}
+        </Grid.Container>
+      </>}
 
-      </Container>
-    </div>
+    </Container>
   )
 }
 
