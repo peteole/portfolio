522d45928bfcc5c7eede2e48bbdc82decf9e09b4
Ole Petersen <peteole2707@gmail.com>
2022-09-16 14:30:55 +0200

    Added experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,7 @@ const Home: NextPage<{ jsonResume: ResumeSchema }> = ({ jsonResume }) => {
       <Container>
         <h1>Hi, I am {jsonResume.basics?.name}</h1>
         <Text>{jsonResume.basics?.summary}</Text>
+
         <Text h2>Education</Text>
         <Grid.Container gap={2} justify="center">
           {jsonResume.education?.map((v, i) => (
@@ -29,18 +30,47 @@ const Home: NextPage<{ jsonResume: ResumeSchema }> = ({ jsonResume }) => {
               <Card isHoverable css={{ width: "400px" }}>
                 <Card.Header>
                   <Col>
-                    <Badge color="primary" >{v.startDate}-{v.endDate}</Badge>
+                    <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
                     <Text h3>{v.institution}</Text>
                   </Col>
                 </Card.Header>
                 <Card.Body>
                   <Text>Score: {v.score}</Text>
-                  {v.courses&&<Text size="small"><Text h4>Courses</Text>{v.courses.map((c,i)=><Badge key={i}>{c}</Badge>)}</Text>}
+                  {v.courses && <Container ><Text h4>Courses</Text>{v.courses.map((c, i) => <Badge key={i}>{c}</Badge>)}</Container>}
                 </Card.Body>
                 {v.url && <Card.Footer><Link href={v.url}>Learn more</Link></Card.Footer>}
               </Card>
             </Grid>))}
         </Grid.Container>
+
+        {jsonResume.work && <>
+          <Text h2>Experience</Text>
+          <Grid.Container gap={2} justify="center">
+            {jsonResume.work.map((v, i) => <>
+              <Grid key={i}>
+                <Card isHoverable css={{ width: "400px" }}>
+                  <Card.Header>
+                    <Col>
+                      <Badge color="primary" >{v.startDate} to {v.endDate || "now"}</Badge>
+                      <Text h3>{v.name}</Text>
+                      <Badge>{v.position}</Badge>
+                    </Col>
+                  </Card.Header>
+                  <Card.Body>
+                    <Text>{v.summary}</Text>
+                    <Text>{v.description}</Text>
+                    {v.highlights && <Container >
+                      <Text h4>Highlights</Text>
+                      {v.highlights.map((c, i) => <Text key={i}>- {c}</Text>)}
+                    </Container>}
+                  </Card.Body>
+                  {v.url && <Card.Footer><Link href={v.url}>Learn more</Link></Card.Footer>}
+                </Card>
+              </Grid>
+            </>)}
+          </Grid.Container>
+        </>}
+
       </Container>
     </div>
   )
