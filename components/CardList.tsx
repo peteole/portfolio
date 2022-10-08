635244ce5d8ce87b57b9cadde9ba0ac14db6a83f
Ole Petersen <peteole2707@gmail.com>
2022-10-08 11:26:49 +0200

    New column layout

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -2,20 +2,30 @@ import { Container, Row } from "@nextui-org/react"
 import React from "react"
 
 const CardList: React.FC<{ children: JSX.Element[] | undefined }> = (props) => {
+    return (
+        <div className="card-container-columns">
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                {props.children?.map((child, index) =>
+                    <div key={index} className="card-container">
+                        {child}
+                    </div>)}
+            </div>
 
-    const versions = []
-    for (const colCount of [1, 2, 3]) {
-        const cols = []
-        for (let i = 0; i < colCount; i++) {
-            cols.push(props.children?.filter((_, j) => j % colCount === i))
-        }
-        versions.push(cols)
-    }
-    return <>
-        {versions.map((cols, v) => <Row key={v} justify="center" className={"card-container card-container-" + v}>
-            {cols.map((col, i) => <div className="card-container-col" key={i}>{col}</div>)}
-        </Row>
-        )}
-    </>
+        </div>
+    )
+    // const versions = []
+    // for (const colCount of [1, 2, 3]) {
+    //     const cols = []
+    //     for (let i = 0; i < colCount; i++) {
+    //         cols.push(props.children?.filter((_, j) => j % colCount === i))
+    //     }
+    //     versions.push(cols)
+    // }
+    // return <>
+    //     {versions.map((cols, v) => <Row key={v} justify="center" className={"card-container card-container-" + v}>
+    //         {cols.map((col, i) => <div className="card-container-col" key={i}>{col}</div>)}
+    //     </Row>
+    //     )}
+    // </>
 }
 export default CardList
